12b6a9352e0bb9f327e428d784f86974a030dd30
mbrechet <mbrechet.ext@orange.com>
2013-12-02 15:29:00 +0100

    correct bash for commit dash

--- svn_commit.sh
+++ svn_commit.sh
@@ -1,9 +1,6 @@
-mkdir release
+svn checkout --username webtvportal-ic https://orangeforge.rd.francetelecom.fr/svnroot/WebTVPortals/client/js/branches/html5_trunk_G01R10/src/main/resources/js/dashjs/ release/
+cp -f dash.all.js release/
+cp -f dash.min.js release/
 cd release
-svn checkout --username webtvportal-ic https://orangeforge.rd.francetelecom.fr/svnroot/WebTVPortals/client/js/branches/html5_trunk_G01R10/src/main/resources/js/dashjs/
-
-cp -f ../dash.all.js .
-cp -f ../dash.min.js .
-
-svn commit --username webtvportal-ic --password welcome0 -m "update Dahs.js" *
-
+svn commit --username webtvportal-ic --password welcome0 -m "update Dash.js" *
+ 
