e5605fec26ce99312dd724cb8ecf692b2604e347
Morgan Gautier <mgautier.ext@orange.com>
2013-12-02 16:10:53 +0100

    enlève pwd et spécifie les fichiers à commiter

--- svn_commit.sh
+++ svn_commit.sh
@@ -1,7 +1,7 @@
-svn checkout --username webtvportal-ic https://orangeforge.rd.francetelecom.fr/svnroot/WebTVPortals/client/js/branches/html5_trunk_G01R10/src/main/resources/js/dashjs/ release/
+svn checkout --username webtvportal-ic https://orangeforge.rd.francetelecom.fr/svnroot/WebTVPortals/client/js/branches/html5_trunk_G01R10/src/main/resources/js/dashjs/ release
 cp -f dash.all.js release/
 cp -f dash.min.js release/
-cd release/
+cd release
 svn commit --username webtvportal-ic --password welcome0 -m "update Dash.min.js" dash.min.js
 svn commit --username webtvportal-ic --password welcome0 -m "update Dash.all.js" dash.all.js
  
