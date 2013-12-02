2a71399a3d90feb1056f4a60b236183fc0c50b16
mbrechet <mbrechet.ext@orange.com>
2013-12-02 15:39:46 +0100

    add pwd to know where is the batch
    Signed-off-by: mbrechet <mbrechet.ext@orange.com>

--- svn_commit.sh
+++ svn_commit.sh
@@ -1,6 +1,7 @@
 svn checkout --username webtvportal-ic https://orangeforge.rd.francetelecom.fr/svnroot/WebTVPortals/client/js/branches/html5_trunk_G01R10/src/main/resources/js/dashjs/ release/
 cp -f dash.all.js release/
 cp -f dash.min.js release/
-cd release
+pwd
+cd release/
 svn commit --username webtvportal-ic --password welcome0 -m "update Dash.js" *
  
