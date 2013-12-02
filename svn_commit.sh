c9474ce3e4d665c3bc639ca6cd71ff6ceb150342
mbrechet <mbrechet.ext@orange.com>
2013-12-02 17:05:51 +0100

    udpate bash svn

--- svn_commit.sh
+++ svn_commit.sh
@@ -1,7 +1,6 @@
 svn checkout --username webtvportal-ic https://orangeforge.rd.francetelecom.fr/svnroot/WebTVPortals/client/js/branches/html5_trunk_G01R10/src/main/resources/js/dashjs/ 
 cp -f dash.all.js ./dashjs/
 cp -f dash.min.js ./dashjs/
-cd dashjs
-svn commit --username webtvportal-ic --password welcome0 -m "update Dash.min.js" dash.min.js
-svn commit --username webtvportal-ic --password welcome0 -m "update Dash.all.js" dash.all.js
+svn commit --username webtvportal-ic --password welcome0 -m "update Dash.min.js" dashjs/dash.min.js
+svn commit --username webtvportal-ic --password welcome0 -m "update Dash.all.js" dashjs/dash.all.js
  
