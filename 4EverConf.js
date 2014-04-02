5745b7578be0944b9af31083c5102d0db470068f
Morgan Gautier <mgautier.ext@orange.com>
2014-04-02 10:46:49 +0200

    [4Ever Page] : BBE real infos for the demo

--- 4EverConf.js
+++ 4EverConf.js
@@ -1,70 +1,51 @@
 
 // /!\ if you modify thi title don't forget to modify the css rule ".reflected:before"
-var pageTitle = "Ultra High Definition HEVC DASH";
+var pageTitle = "<b>UHDTV</b> in an <b>HTML5</b> browser with <b>DASH-HEVC</b> content adaptation";
 
 
 // if you want to have multiple lines : add <br> tag :)
 var infosTitle = "About this demo";
-var infosContent = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+var infosContent = 
+    "•  I want to watch an UltraHD content found on the Internet<br><br>" +
+    "•  I take my laptop, my tablet or my connected TV<br><br>" +
+    "•  I start my favorite HMTL5 browser<br>   => the video starts without any plugin<br><br>" +
+    "•  When the video plays, it automatically adapts to network conditions<br>   => no image freezing";
 
 // maximum 9 streams
 var videoStreams = [
     {
-        "name": "DASH HEVC UHD",
+        "name": "DASH-HEVC UHD",
         "url": "http://10.194.60.47/orange3s/UHD/dashevc-live-2s/dashevc-live-2s-4k.mpd",
         "image": "app/img/dashevc-live-2s.jpg",
         "representations": [
-            "3840x2160 - 60 Hz - 10.6 Mbps",
+            "3840x2160 - 60 Hz - 18.4 Mbps",
             "3840x2160 - 60 Hz - 13.5 Mbps",
-            "3840x2160 - 60 Hz - 18.4 Mbps"
+            "3840x2160 - 60 Hz - 10.6 Mbps"
         ]
     },
     {
-        "name": "DASH HEVC HD",
+        "name": "DASH-HEVC HD",
         "url": "http://10.194.60.47/orange3s/UHD/dashevc-live-2s/dashevc-live-2s-p60.mpd",
         "image": "app/img/dashevc-live-2s.jpg",
-        "representations" : [
-            "ceci est une info qui ne sert à rien ...."
-        ]
-    },
-    {
-        "name": "DASH HEVC HD",
-        "url": "http://2is7server1.rd.francetelecom.com/VOD/BBB-SD/big_buck_bunny_1080p_stereo.ism/Manifest",
-        "image": "",
         "representations": [
-            "Coucou Bertrand",
-            "Comment ça va ?",
-            "Tu t'en sors avec la 4K ?"
+            "1920x1080 - 60 Hz - 7.5 Mbps",
+            "1920x1080 - 60 Hz - 5.5 Mbps",
+            "1280x720  - 60 Hz - 3.7 Mbps",
+            "1280x720  - 60 Hz - 2.7 Mbps"
         ]
     },
     {
-        "name": "DASH HEVC HD",
-        "url": "http://2is7server1.rd.francetelecom.com/C4/C4-46_S2.isml/Manifest",
-        "image": "app/img/dashevc-live-2s.jpg"
-    },
-    {
-        "name": "DASH HEVC HD",
-        "url": "http://10.194.60.47/orange3s/UHD/dashevc-live-2s/dashevc-live-2s-p60.mpd",
-        "image": "app/img/dashevc-live-2s.jpg"
-    },
-    {
-        "name": "DASH HEVC HD",
-        "url": "http://10.194.60.47/orange3s/UHD/dashevc-live-2s/dashevc-live-2s-p60.mpd",
-        "image": "app/img/dashevc-live-2s.jpg"
-    },
-    {
-        "name": "DASH HEVC HD",
-        "url": "http://10.194.60.47/orange3s/UHD/dashevc-live-2s/dashevc-live-2s-p60.mpd",
-        "image": "app/img/dashevc-live-2s.jpg"
-    },
-    {
-        "name": "DASH HEVC HD",
-        "url": "http://10.194.60.47/orange3s/UHD/dashevc-live-2s/dashevc-live-2s-p60.mpd",
-        "image": "app/img/dashevc-live-2s.jpg"
-    },
-    {
-        "name": "DASH HEVC UHD/HD",
+        "name": "DASH-HEVC UHD/HD",
         "url": "http://10.194.60.47/orange3s/UHD/dashevc-live-2s/dashevc-live-2s.mpd",
-        "image": ""
+        "image": "app/img/dashevc-live-2s.jpg",
+        "representations": [
+            "3840x2160 - 60 Hz - 18.4 Mbps",
+            "3840x2160 - 60 Hz - 13.5 Mbps",
+            "3840x2160 - 60 Hz - 10.6 Mbps",
+            "1920x1080 - 60 Hz - 7.5 Mbps",
+            "1920x1080 - 60 Hz - 5.5 Mbps",
+            "1280x720  - 60 Hz - 3.7 Mbps",
+            "1280x720  - 60 Hz - 2.7 Mbps"
+        ]
     }
 ];
